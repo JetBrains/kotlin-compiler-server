32a891a5a3f3858d0b4553f0bd9cb3a1b41fd1a2
Prendota <Prendota@mail.ru>
2019-11-08 20:14:52 +0300

    fix(analyze): correct filter

--- src/main/kotlin/com/compiler/server/compiler/components/ErrorAnalyzer.kt
+++ src/main/kotlin/com/compiler/server/compiler/components/ErrorAnalyzer.kt
@@ -73,7 +73,7 @@ class ErrorAnalyzer(private val kotlinEnvironment: KotlinEnvironment) {
     }.toMap()
   }
 
-  fun isOnlyWarnings(errors: Map<String, List<ErrorDescriptor>>) = errors.none { it.value.none { error -> error.severity == Severity.ERROR } }
+  fun isOnlyWarnings(errors: Map<String, List<ErrorDescriptor>>) = errors.none { it.value.any { error -> error.severity == Severity.ERROR } }
 
   private fun anylizeErrorsFrom(file: PsiFile): List<ErrorDescriptor> {
     class Visitor : PsiElementVisitor() {
