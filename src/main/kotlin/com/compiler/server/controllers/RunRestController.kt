ab6caba902d114b6bee854b956549342e3be9397
Prendota <Prendota@mail.ru>
2019-11-17 00:28:58 +0300

    feat: support kotlin-playground API

--- src/main/kotlin/com/compiler/server/controllers/RunRestController.kt
+++ src/main/kotlin/com/compiler/server/controllers/RunRestController.kt
@@ -3,6 +3,8 @@ package com.compiler.server.controllers
 import com.compiler.server.compiler.components.KotlinProjectExecutor
 import com.compiler.server.compiler.model.ErrorDescriptor
 import com.compiler.server.compiler.model.Project
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
@@ -26,4 +28,43 @@ class RunRestController(private val kotlinProjectExecutor: KotlinProjectExecutor
   @PostMapping("/api/compiler/highlight")
   fun highlightEndpoint(@RequestBody project: Project): Map<String, List<ErrorDescriptor>> = kotlinProjectExecutor.highlight(project)
 
+
+  /**
+   * Endpoint for support requests from kotlin playground client.
+   * Kotlin Playground component see: https://github.com/JetBrains/kotlin-playground
+   * Old server see: https://github.com/JetBrains/kotlin-web-demo
+   */
+  @PostMapping(
+    value = ["/kotlinServer"],
+    consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE],
+    produces = [MediaType.APPLICATION_JSON_VALUE]
+  )
+  @Suppress("IMPLICIT_CAST_TO_ANY")
+  fun tryKotlinLangObsoleteEndpoint(
+    @RequestParam type: String,
+    @RequestParam(required = false) line: Int?,
+    @RequestParam(required = false) ch: Int?,
+    @RequestParam(required = false, defaultValue = "java") runConf: String,
+    @RequestParam project: Project
+  ): ResponseEntity<*> {
+    val result = when (type) {
+      "run" -> {
+        when (runConf) {
+          "java" -> executeKotlinProjectEndpoint(project)
+          "js" -> translateKotlinProjectEndpoint(project)
+          "canvas" -> translateKotlinProjectEndpoint(project)
+          else -> error("Unknown 'runCong' $runConf")
+        }
+      }
+      "highlight" -> highlightEndpoint(project)
+      "complete" -> {
+        if (line != null && ch != null) {
+          getKotlinCompleteEndpoint(project, line, ch)
+        }
+        else throw error("No parameters 'line' or 'ch'")
+      }
+      else -> throw error("No parameter 'type' found")
+    }
+    return ResponseEntity.ok(result)
+  }
 }
