bfe4a126f18019d539e8cc827c2e474ffcb03889
Prendota <Prendota@mail.ru>
2019-11-16 18:42:12 +0300

    test: separate js & jvm

--- src/test/kotlin/com/compiler/server/ConvertToJsRunnerTest.kt
+++ src/test/kotlin/com/compiler/server/ConvertToJsRunnerTest.kt
@@ -6,39 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class BaseRunnerTest {
-
+class ConvertToJsRunnerTest {
   @Autowired
   private lateinit var testRunner: TestProjectRunner
 
   @Test
-  fun `base execute test JVM`() = testRunner.run(
-    code = "fun main() {\n println(\"Hello, world!!!\")\n}",
-    contains = "Hello, world!!!"
-  )
-
-  @Test
-  fun `base execute test JVM multi`() = testRunner.multiRun(
-    code = listOf(
-      "import cat.Cat\n\nfun main(args: Array<String>) {\nval cat = Cat(\"Kitty\")\nprintln(cat.name)\n}",
-      "package cat\n    class Cat(val name: String)"
-    ),
-    contains = "Kitty"
-  )
-
-  @Test
-  fun `base execute test JS`() = testRunner.runJs(
+  fun `base execute test`() = testRunner.runJs(
     code = "fun main() {\n println(\"Hello, world!!!\")\n}",
     contains = "println('Hello, world!!!');"
   )
 
   @Test
-  fun `base execute test JS multi`() = testRunner.multiRunJs(
+  fun `base execute test multi`() = testRunner.multiRunJs(
     code = listOf(
       "import cat.Cat\n\nfun main(args: Array<String>) {\nval cat = Cat(\"Kitty\")\nprintln(cat.name)\n}",
       "package cat\n    class Cat(val name: String)"
     ),
     contains = "var cat = new Cat('Kitty');"
   )
-
 }
---
+++ src/test/kotlin/com/compiler/server/JvmRunnerTest.kt
@@ -0,0 +1,29 @@
+package com.compiler.server
+
+import com.compiler.server.generator.TestProjectRunner
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JvmRunnerTest {
+
+  @Autowired
+  private lateinit var testRunner: TestProjectRunner
+
+  @Test
+  fun `base execute test JVM`() = testRunner.run(
+    code = "fun main() {\n println(\"Hello, world!!!\")\n}",
+    contains = "Hello, world!!!"
+  )
+
+  @Test
+  fun `base execute test JVM multi`() = testRunner.multiRun(
+    code = listOf(
+      "import cat.Cat\n\nfun main(args: Array<String>) {\nval cat = Cat(\"Kitty\")\nprintln(cat.name)\n}",
+      "package cat\n    class Cat(val name: String)"
+    ),
+    contains = "Kitty"
+  )
+
+}
