a584292a5567a44b2a8cd3cf3675596e8c07b00e
Prendota <Prendota@mail.ru>
2019-11-18 00:35:56 +0300

    test: add for interrupt conditions

--- src/main/kotlin/com/compiler/server/executor/ExecutorMessages.kt
+++ src/main/kotlin/com/compiler/server/executor/ExecutorMessages.kt
@@ -1,6 +1,6 @@
 package com.compiler.server.executor
 
 object ExecutorMessages {
-  const val TIMEOUT_MESSAGE = "evaluation stopped while it's taking too long"
+  const val TIMEOUT_MESSAGE = "evaluation stopped while it's taking too long ⌛️"
   const val TOO_LONG_OUTPUT_MESSAGE = "Evaluation stopped while log size exceeded max size"
 }
