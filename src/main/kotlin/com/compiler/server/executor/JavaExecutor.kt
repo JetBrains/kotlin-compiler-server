dd31a8e9847684d27d93319881c44ed778a9e00a
wayfarer-rus <dr.traveller@gmail.com>
2019-11-24 17:46:37 +0300

    chore: process interrupt improvement

--- src/main/kotlin/com/compiler/server/executor/JavaExecutor.kt
+++ src/main/kotlin/com/compiler/server/executor/JavaExecutor.kt
@@ -2,7 +2,6 @@ package com.compiler.server.executor
 
 import com.compiler.server.model.ExceptionDescriptor
 import com.compiler.server.model.JavaExecutionResult
-import com.compiler.server.utils.escapeString
 import org.springframework.stereotype.Component
 import java.io.BufferedReader
 import java.io.IOException
@@ -11,22 +10,26 @@ import java.nio.file.Path
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
-import java.util.concurrent.locks.ReentrantLock
-
-const val MAX_OUTPUT_SIZE = 100 * 1024
-const val EXECUTION_TIMEOUT = 10000L
 
 @Component
 class JavaExecutor {
+
+  // TODO:: move them into application properties
+  companion object {
+    const val MAX_OUTPUT_SIZE = 100 * 1024
+    const val EXECUTION_TIMEOUT = 10000L
+  }
 
   data class ProgramOutput(
     val standardOutput: String,
     val errorOutput: String,
     val exception: Exception? = null
   ) {
+    constructor(exception: Exception) : this("", "", exception)
+
     fun asExecutionResult(): JavaExecutionResult {
       return JavaExecutionResult(
-        text = "<outStream>$standardOutput\n</outStream>",
+        text = "<outStream>$standardOutput\n</outStream><errStream>$errorOutput</errStream>",
         exception = exception?.let {
           ExceptionDescriptor(it.message ?: "no message", it::class.java.toString())
         })
@@ -35,64 +38,86 @@ class JavaExecutor {
 
   fun execute(args: List<String>): JavaExecutionResult {
     return Runtime.getRuntime().exec(args.toTypedArray()).use {
+      // why?
       outputStream.close()
-      val interruptCondition = ProcessInterruptCondition()
+
       val standardOut = InputStreamReader(this.inputStream).buffered()
       val standardError = InputStreamReader(this.errorStream).buffered()
-      val errorText = StringBuilder()
-      val standardText = StringBuilder()
-      val standardThread = appendTo(standardText, standardOut, interruptCondition)
-      standardThread.start()
-      val errorThread = appendTo(errorText, standardError, interruptCondition)
-      errorThread.start()
 
-      val interruptMsg = Executors.newSingleThreadExecutor().submit(
-        interruptAfter(
-          process = this,
-          threads = listOf(standardThread, errorThread),
-          interruptCondition = interruptCondition
-        )
-      )
-      val interruptType: ConditionType
+      val standardOutput = asyncBufferedOutput(standardOut, limit = MAX_OUTPUT_SIZE)
+      val errorOutput = asyncBufferedOutput(standardError, limit = MAX_OUTPUT_SIZE)
+
       try {
-        waitFor()
-        interruptCondition.exitNow()
-        interruptType = interruptMsg.get()
-        standardThread.join(10000)
-        errorThread.join(10000)
+        val futuresList = Executors.newFixedThreadPool(2) // one thread per output
+                .invokeAll(listOf(standardOutput, errorOutput), EXECUTION_TIMEOUT, TimeUnit.MILLISECONDS)
+        // we do not wait for process to end, while either time-limit or output-limit triggered
+        // program itself will be destroyed right after we'll leave this method
+
+        val outputResults = futuresList.map {
+          try {
+            it.get()
+          } catch (_: Exception) {
+            ""
+          }
+        }
+
+        val (standardText, errorText) = outputResults
+        val exception = if (errorText.isNotEmpty()) Exception(errorText) else null
+
+        when {
+            futuresList.any { !it.isDone } -> {
+              // execution timeout. Both Future objects must be in 'done' state, to say that process finished
+              ProgramOutput(
+                      ExecutorMessages.TIMEOUT_MESSAGE,
+                      errorText,
+                      exception
+              ).asExecutionResult()
+            }
+            outputResults.any {it.length >= MAX_OUTPUT_SIZE} -> {
+              // log-limit exceeded
+              ProgramOutput(
+                      ExecutorMessages.TOO_LONG_OUTPUT_MESSAGE,
+                      errorText,
+                      exception
+              ).asExecutionResult()
+            }
+            else -> {
+              // normal exit
+              ProgramOutput(standardText, errorText, exception).asExecutionResult()
+            }
+        }
+      } catch (any: Exception) {
+        // all sort of things may happen, so we better be aware
+        any.printStackTrace()
+        ProgramOutput(any).asExecutionResult()
       }
       finally {
         try {
           standardOut.close()
           standardError.close()
         }
-        catch (e: IOException) {
-          e.printStackTrace()
+        catch (_: IOException) {
+          // don't care
         }
-      }
-      val exception = if (errorText.toString().isNotEmpty()) {
-        Exception(errorText.toString())
-      }
-      else null
-      when (interruptType) {
-        ConditionType.NORMAL -> ProgramOutput(
-          standardText.toString(),
-          errorText.toString(),
-          exception
-        ).asExecutionResult()
-        ConditionType.LOG_SIZE -> ProgramOutput(
-          ExecutorMessages.TOO_LONG_OUTPUT_MESSAGE,
-          errorText.toString(),
-          exception
-        ).asExecutionResult()
-        ConditionType.TIMEOUT -> ProgramOutput(
-          ExecutorMessages.TIMEOUT_MESSAGE,
-          errorText.toString(),
-          exception
-        ).asExecutionResult()
       }
     }
   }
+
+  private fun asyncBufferedOutput(standardOut: BufferedReader, limit: Int): Callable<String> = Callable {
+    val output = StringBuilder()
+    try {
+      while (output.length < limit) {
+        val line = standardOut.readLine()
+        if (line == null) break
+        else output.append(line)
+      }
+    } catch (_: Exception) {
+      // something happened with the stream. Just return what we've collected so far
+    }
+
+    output.toString()
+  }
+
 
   private fun <T> Process.use(body: Process.() -> T) = try {
     body()
@@ -101,78 +126,6 @@ class JavaExecutor {
     destroy()
   }
 
-  private fun interruptAfter(
-    process: Process,
-    threads: List<Thread>,
-    interruptCondition: ProcessInterruptCondition
-  ): Callable<ConditionType> = Callable {
-    val result = interruptCondition.waitForCondition(EXECUTION_TIMEOUT)
-    threads.forEach { it.interrupt() }
-    process.destroy()
-    result
-  }
-
-  private fun appendTo(
-    string: StringBuilder,
-    from: BufferedReader,
-    interruptCondition: ProcessInterruptCondition
-  ) = Thread {
-    try {
-      while (true) {
-        val line = from.readLine()
-        if (Thread.interrupted() || line == null) break
-        interruptCondition.appendCharacterCounter(line.length)
-        string.appendln(escapeString(line))
-      }
-    }
-    catch (e: Throwable) {
-      if (!Thread.interrupted()) {
-        e.printStackTrace()
-      }
-    }
-  }
-}
-
-class ProcessInterruptCondition {
-  private lateinit var conditionBreak: ConditionType
-  private var totalCharactersOutput: Int = 0
-  private val lock = ReentrantLock()
-  private val condition = lock.newCondition()
-
-  fun waitForCondition(delay: Long): ConditionType = withLock {
-    if (!condition.await(delay, TimeUnit.MILLISECONDS))
-      return@withLock ConditionType.TIMEOUT
-    return@withLock conditionBreak
-  }
-
-  fun appendCharacterCounter(length: Int) = withLock {
-    this.totalCharactersOutput += length
-    if (totalCharactersOutput > MAX_OUTPUT_SIZE) {
-      this.conditionBreak = ConditionType.LOG_SIZE
-      condition.signal()
-    }
-  }
-
-  fun exitNow() = withLock {
-    this.conditionBreak = ConditionType.NORMAL
-    condition.signal()
-  }
-
-  private fun <T> withLock(block: () -> T): T {
-    lock.lock()
-    try {
-      return block()
-    }
-    finally {
-      lock.unlock()
-    }
-  }
-}
-
-enum class ConditionType {
-  TIMEOUT,
-  LOG_SIZE,
-  NORMAL
 }
 
 class JavaArgumentsBuilder(
