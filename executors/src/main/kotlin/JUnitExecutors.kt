b05b75f2648194afaa4cda25abf6e73f39d5d1e1
Prendota <Prendota@mail.ru>
2019-12-09 19:28:57 +0300

    test: use Class.forName instead of ClassLoader

--- executors/src/main/kotlin/JUnitExecutors.kt
+++ executors/src/main/kotlin/JUnitExecutors.kt
@@ -7,7 +7,6 @@ import org.junit.Test
 import org.junit.runner.JUnitCore
 import org.junit.runner.Request
 import java.io.File
-import java.net.URLClassLoader
 import java.util.*
 
 class JUnitExecutors {
@@ -49,23 +48,19 @@ class JUnitExecutors {
       }
     }
 
-
     private fun loadTestClasses(path: String): List<Class<out Any>> {
       val files = File(path).listFiles().orEmpty()
-      val urls = files.mapNotNull { it.toURI().toURL() }.toTypedArray()
-      val loader = URLClassLoader(urls)
       val names = files
         .filter { it.name.endsWith(".class") }
         .map { it.name.removeSuffix(".class") }
       return names.mapNotNull {
         try {
-          loader.loadClass(it)
+          Class.forName(it)
         }
         catch (_: Exception) {
           null
         }
       }.filter { it -> it.methods.any { it.isAnnotationPresent(Test::class.java) } }
-        .also { loader.close() }
     }
   }
 }
