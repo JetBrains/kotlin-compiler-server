a9e828012c1d6bb84cd248b82d2abb4c87160b8a
Prendota <Prendota@mail.ru>
2019-12-10 17:55:08 +0300

    test: add for koans

--- src/test/kotlin/com/compiler/server/JUnitTestsRunnerTest.kt
+++ src/test/kotlin/com/compiler/server/JUnitTestsRunnerTest.kt
@@ -1,7 +1,6 @@
 package com.compiler.server
 
 import com.compiler.server.generator.TestProjectRunner
-import com.compiler.server.model.TestDescription
 import com.compiler.server.model.TestStatus
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -9,19 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class JUnitTestsRunnerTest {
+class JUnitTestsRunnerTest : BaseJUnitTest() {
+
   @Autowired
   private lateinit var testRunner: TestProjectRunner
-
-  @Test
-  fun `base junit test`() {
-    val test = testRunner.test(
-      "fun start(): String = \"OK\"",
-      "import org.junit.Assert\nimport org.junit.Test\n\nclass TestStart {\n    @Test fun testOk() {\n        Assert.assertEquals(\"OK\", start())\n    }\n}",
-      koansUtilsFile
-    )
-    successTestCheck(test)
-  }
 
   @Test
   fun `base fail junit test`() {
@@ -38,16 +28,4 @@ class JUnitTestsRunnerTest {
       Assertions.assertTrue(it.expected == "OK")
     }
   }
-
-
-  fun successTestCheck(testResults: List<TestDescription>) {
-    testResults
-      .map { it.status }
-      .forEach {
-        Assertions.assertTrue(it == TestStatus.OK)
-      }
-  }
-
-
-  private val koansUtilsFile = "package koans.util\n\nfun String.toMessage() = \"The function '\$this' is implemented incorrectly\"\n\nfun String.toMessageInEquals() = toMessage().inEquals()\n\nfun String.inEquals() = this"
 }
