8ec32d1406f22fb9b16eab5b14a1534af8cf9424
Prendota <Prendota@mail.ru>
2020-01-24 21:29:35 +0300

    chore(version): update kotlin jvm plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ val copyJSDependencies by tasks.creating(Copy::class) {
 plugins {
     id("org.springframework.boot") version "2.2.0.RELEASE"
     id("io.spring.dependency-management") version "1.0.8.RELEASE"
-    kotlin("jvm") version "1.3.60"
+    kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.50"
 }
 
