7384ce209adc2c1596ef763a5f7df1608eee312e
Prendota <Prendota@mail.ru>
2020-01-25 13:54:54 +0300

    chore(deps): spring plugin 1.3.61

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ plugins {
     id("org.springframework.boot") version "2.2.0.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version "1.3.61"
-    kotlin("plugin.spring") version "1.3.50"
+    kotlin("plugin.spring") version "1.3.61"
 }
 
 allprojects {
