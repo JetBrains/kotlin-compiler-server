b2f6c687e7c9b186d68b869dde1f9871c9224107
Prendota <Prendota@mail.ru>
2020-04-24 09:50:05 +0300

    chore(cors): add configuration
    (cherry picked from commit 0dcddb013a3125d9c72081bb7f0c4e9c72888793)

---
+++ src/main/kotlin/com/compiler/server/configuration/CorsConfiguration.kt
@@ -0,0 +1,25 @@
+package com.compiler.server.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+val ACCESS_CONTROL_ALLOW_ORIGIN_VALUE: String = System.getProperty("ACCESS_CONTROL_ALLOW_ORIGIN_VALUE", "*")
+
+@Configuration
+class CorsConfiguration {
+  @Bean
+  fun corsFilter(): CorsFilter {
+    val source = UrlBasedCorsConfigurationSource()
+    val config = CorsConfiguration().apply {
+      addAllowedHeader(ACCESS_CONTROL_ALLOW_ORIGIN_VALUE)
+      addAllowedOrigin(ACCESS_CONTROL_ALLOW_ORIGIN_VALUE)
+      addAllowedMethod("GET")
+      addAllowedMethod("POST")
+    }
+    source.registerCorsConfiguration("/**", config)
+    return CorsFilter(source)
+  }
+}
