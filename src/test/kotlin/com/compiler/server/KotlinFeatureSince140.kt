49a9b2aaaa18ddb23c823bff8d6979cf34e20a56
Pavel Semyonov <pavel.semyonov@jetbrains.com>
2020-04-30 19:17:27 +0700

    test: add tests from 1.4-M2 stdlib blog post

--- src/test/kotlin/com/compiler/server/KotlinFeatureSince140.kt
+++ src/test/kotlin/com/compiler/server/KotlinFeatureSince140.kt
@@ -80,4 +80,170 @@ class KotlinFeatureSince140 : BaseExecutorTest() {
       contains = "true\ntrue"
     )
   }
+
+  @Test
+  fun `blog stdlib 14M2 appendLine`() {
+    run(
+      code = """
+          fun main() {
+          //sampleStart
+            println(buildString {
+              appendLine("Hello,")
+              appendLine("Kotlin 1.4-M2")
+            })
+        //sampleEnd
+        }
+        """.trimIndent(),
+      contains = "Hello,Kotlin 1.4-M2"
+    )
+  }
+  @Test
+  fun `blog stdlib 14M2 arrays oneach shuffle`() {
+    run(
+      code = """
+         fun main() {
+            //sampleStart
+                var language = ""
+                val letters = arrayOf("k", "o", "t", "l", "i", "n")
+                val fileExt = letters.onEach { language += it }
+                   .filterNot { it in "aeuio" }.take(2)
+                   .joinToString(prefix = ".", separator = "")
+                println(language) // "kotlin"
+                println(fileExt) // ".kt"
+            
+                letters.shuffle()
+                println(letters.contentToString())
+            //sampleEnd
+            }
+        """.trimIndent(),
+      contains = "kotlin\n.kt"
+    )
+  }
+  @Test
+  fun `blog stdlib 14M2 arrays sort`() {
+    run(
+      code = """
+         fun main() {
+        //sampleStart
+            val letters = arrayOf("t", "o", "n", "i", "l", "k")
+            letters.reverse(2, 6)
+            letters.sortDescending(0, 3)
+            println(letters.contentToString()) // [k, o, t, l, i, n]
+        //sampleEnd
+        }
+        """.trimIndent(),
+      contains = "kotlin"
+    )
+  }
+  @Test
+  fun `blog stdlib 14M2 setOfNotNull`() {
+    run(
+      code = """
+        fun main() {
+        //sampleStart
+            val set = setOfNotNull(null, 1, 2, 0, null)
+            println(set)
+        //sampleEnd
+        }
+        """.trimIndent(),
+      contains = "[1, 2, 0]"
+    )
+  }
+  @Test
+  fun `blog stdlib 14M2 indexed`() {
+    run(
+      code = """
+        fun main() {
+        //sampleStart
+            val list = mutableListOf("a", "b", "c", "d").onEachIndexed
+            { index, item -> println("Item at ${'$'}index is ${'$'}item") }
+        
+            val emptyList = emptyList<Int>()
+            emptyList.reduceIndexedOrNull {index, a, b -> index + a + b} // null
+        //sampleEnd
+        }
+        """.trimIndent(),
+      contains = "Item at 0 index is a"
+    )
+  }
+  @Test
+  fun `blog stdlib 14M2 runningFold Reduce`() {
+    run(
+      code = """
+        fun main() {
+        //sampleStart
+            val numbers = mutableListOf(0, 1, 2, 3, 4, 5)
+            val runningReduceSum = numbers.runningReduce() { sum, item -> sum + item} // [0, 1, 3, 6, 10, 15]
+            val runningFoldSum = numbers.runningFold(10) { sum, item -> sum + item} // [10, 10, 11, 13, 16, 20, 25]
+        //sampleEnd
+        }
+        """.trimIndent(),
+      contains = ""
+    )
+  }
+  @Test
+  fun `blog stdlib 14M2 accept null`() {
+    run(
+      code = """
+        fun main() {
+        //sampleStart
+           val s: String? = null
+           println(s.toBoolean())  // false
+        //sampleEnd
+        }
+        """.trimIndent(),
+      contains = "false"
+    )
+  }
+  @Test
+  fun `blog stdlib 14M2 double float constants`() {
+    run(
+      code = """
+        fun main() {
+        //sampleStart
+            println(Double.NaN)  // NaN
+            println(Double.NEGATIVE_INFINITY)  // -Infinity
+            println(Double.MAX_VALUE < Double.POSITIVE_INFINITY)  // true
+            println(Double.SIZE_BITS) // 64
+        //sampleEnd
+        }
+        """.trimIndent(),
+      contains = "NaN\n-Infinity\ntrue\n64"
+    )
+  }
+  @Test
+  fun `blog stdlib 14M2 maxOf vararg`() {
+    run(
+      code = """
+        fun main() {
+        //sampleStart
+            val max = maxOf(1, 2, 3, 4)
+            println(max)
+        //sampleEnd
+        }
+        """.trimIndent(),
+      contains = "4"
+    )
+  }
+  @Test
+  fun `blog stdlib 14M2 delegate`() {
+    run(
+      code = """
+        class MyClass {
+           var newName: Int = 0
+           @Deprecated("Use 'newName' instead", ReplaceWith("newName"))
+           var oldName: Int by this::newName
+        }
+        
+        fun main() {
+           val myClass = MyClass()
+           // Notification: 'oldName: Int' is deprecated.
+           // Use 'newName' instead
+           myClass.oldName = 42
+           println(myClass.newName) // 42
+        }
+        """.trimIndent(),
+      contains = "42"
+    )
+  }
 }
