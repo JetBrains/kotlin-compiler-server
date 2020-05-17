eef04094bce9dfa260298cf86d988fc4d89817e9
Konstantin Lyubort <lyubortk@gmail.com>
2020-05-18 02:04:16 +0300

    fix error messages

--- src/main/kotlin/com/compiler/server/executor/ExecutorMessages.kt
+++ src/main/kotlin/com/compiler/server/executor/ExecutorMessages.kt
@@ -1,7 +1,7 @@
 package com.compiler.server.executor
 
 object ExecutorMessages {
-  const val TIMEOUT_MESSAGE = "Evaluation stopped while it's taking too long️"
-  const val TOO_LONG_OUTPUT_MESSAGE = "Evaluation stopped while log size exceeded max size"
+  const val TIMEOUT_MESSAGE = "Evaluation was stopped as exceeding time limit"
+  const val TOO_LONG_OUTPUT_MESSAGE = "Evaluation was stopped as exceeding maximum output size"
   const val NO_COMPILERS_FILE_FOUND = "No compilation files found!"
 }
