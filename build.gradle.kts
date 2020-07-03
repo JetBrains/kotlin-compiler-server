e18170742cddbae29dd7ad9109caa5698e34eef5
Stanislav Erokhin <Stanislav.Erokhin@jetbrains.com>
2020-07-03 12:48:48 +0300

    Advance kotlin that are used for project compilation
    It was done due to M2 do not support overloading by
    return type, that was used for flatMap was resolved.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,8 @@ val copyJSDependencies by tasks.creating(Copy::class) {
 plugins {
     id("org.springframework.boot") version "2.2.0.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    kotlin("jvm") version "1.4-M2-release-92"
-    kotlin("plugin.spring") version "1.4-M2-release-92"
+    kotlin("jvm") version "1.4-M3-release-207"
+    kotlin("plugin.spring") version "1.4-M3-release-207"
 }
 
 allprojects {
