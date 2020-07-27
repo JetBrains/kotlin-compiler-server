7287de0b1b51ce0575d7744df855ad2e783a8bff
PolinB <PolinB2413@gmail.com>
2020-07-27 12:40:42 +0300

    Code refactoring. Added comments for exceptions and errors.

--- src/main/kotlin/com/compiler/server/compiler/components/SuggestionProvider.kt
+++ src/main/kotlin/com/compiler/server/compiler/components/SuggestionProvider.kt
@@ -31,8 +31,10 @@ import org.jetbrains.kotlin.types.asFlexibleType
 import org.jetbrains.kotlin.types.isFlexible
 import org.springframework.stereotype.Component
 import java.io.File
+import java.lang.reflect.Method
 import java.lang.reflect.Modifier
 import java.util.jar.JarFile
+import kotlin.reflect.KFunction
 import kotlin.reflect.KVisibility
 import kotlin.reflect.jvm.internal.KotlinReflectionInternalError
 import kotlin.reflect.jvm.kotlinFunction
@@ -291,105 +293,6 @@ class SuggestionProvider(
       importableFqName?.asString() in excludedFromCompletion
   }
 
-  private fun getVariantsForZip(file: File): List<ImportInfo> {
-    val jarFile = JarFile(file)
-    val allClasses = hashSetOf<ImportInfo>()
-    jarFile.entries().toList().map { entry ->
-      if (!entry.isDirectory
-        && entry.name.endsWith(".class")
-      ) {
-        val name = entry.name.removeSuffix(".class")
-        val fullName = name.replace("/", ".")
-        if (fullName != MODULE_INFO_NAME) {
-          try {
-            val clazz = ClassLoader.getSystemClassLoader().loadClass(fullName)
-            allClasses.addAll(allFunctionsFromClass(clazz))
-            if (clazz.isKotlinClass()) {
-              val kotlinClass = clazz.kotlin
-              try {
-                kotlinClass.nestedClasses.filter {
-                  it.visibility == KVisibility.PUBLIC
-                }.map {
-                  val canonicalName = it.qualifiedName ?: ""
-                  val simpleName = it.simpleName ?: ""
-                  val importInfo = ImportInfo(canonicalName, simpleName)
-                  allClasses.add(importInfo)
-                }
-                if (kotlinClass.visibility == KVisibility.PUBLIC) {
-                  val canonicalName = kotlinClass.qualifiedName ?: ""
-                  val simpleName = kotlinClass.simpleName ?: ""
-                  val importInfo = ImportInfo(canonicalName, simpleName)
-                  allClasses.add(importInfo)
-                }
-              } catch (err: UnsupportedOperationException) {
-                allClasses.addAll(allClassesFromJavaClass(clazz))
-              } catch (err: AssertionError) {
-//              println("ERROR_ASSERTION: $fullName")
-                allClasses.addAll(allClassesFromJavaClass(clazz))
-              } catch (err: IncompatibleClassChangeError) {
-//              println("ERROR_INCOMPATIBLE_CLASS_CHANGE: $fullName")
-                allClasses.addAll(allClassesFromJavaClass(clazz))
-              } catch (err: InternalError) {
-//              println("ERROR_INTERNAL: $fullName")
-                allClasses.addAll(allClassesFromJavaClass(clazz))
-              } catch (err: KotlinReflectionInternalError) {
-                allClasses.addAll(allClassesFromJavaClass(clazz))
-              } catch (err: Exception) {
-                allClasses.addAll(allClassesFromJavaClass(clazz))
-              }
-            } else {
-              allClasses.addAll(allClassesFromJavaClass(clazz))
-            }
-          } catch (error: VerifyError) {
-//          println("ERROR_VERIFY: $fullName")
-          } catch (error: Throwable) {
-            println("ERROR: $fullName")
-          }
-        }
-      }
-    }
-    return allClasses.toList()
-  }
-
-  private fun allFunctionsFromClass(clazz: Class<*>): HashSet<ImportInfo> {
-    val allClasses = hashSetOf<ImportInfo>()
-    clazz.declaredMethods.map { method ->
-      try {
-        val kotlinFunction = method.kotlinFunction
-        val importInfo: ImportInfo? =
-          if (kotlinFunction != null
-            && kotlinFunction.visibility == KVisibility.PUBLIC) {
-            importInfoByMethodAndParent(
-              kotlinFunction.name,
-              kotlinFunction.parameters.map { it.type }.joinToString(),
-              clazz)
-          } else if (kotlinFunction == null
-            && Modifier.isPublic(method.modifiers) &&
-            Modifier.isStatic(method.modifiers)) {
-            importInfoByMethodAndParent(method.name, method.parameters.map { it.type.name }.joinToString(), clazz)
-          } else {
-            null
-          }
-        if (importInfo != null) allClasses.add(importInfo)
-      } catch (err: KotlinReflectionInternalError) {
-        // do nothing
-      } catch (err: UnsupportedOperationException) {
-        // check like Java
-      } catch (err: AssertionError) {
-        // do nothing
-      }
-    }
-    return allClasses
-  }
-
-
-  private fun importInfoByMethodAndParent(methodName: String, parametersString: String, parent: Class<*>): ImportInfo {
-    val shortName = methodName.split("$").first()
-    val className = "$shortName($parametersString)"
-    val fullName = "${parent.`package`.name}.$shortName"
-    return ImportInfo(fullName, className)
-  }
-
   private fun allClassesFromJavaClass(clazz: Class<*>): HashSet<ImportInfo> {
     val allClasses = hashSetOf<ImportInfo>()
     clazz.classes.filter {
@@ -401,6 +304,144 @@ class SuggestionProvider(
       allClasses.add(importInfo)
     }
     return allClasses
+  }
+
+  private fun allClassesFromKotlinClass(clazz: Class<*>): HashSet<ImportInfo> {
+    val allClasses = hashSetOf<ImportInfo>()
+    val kotlinClass = clazz.kotlin
+    try {
+      kotlinClass.nestedClasses.filter {
+        it.visibility == KVisibility.PUBLIC
+      }.map {
+        val canonicalName = it.qualifiedName ?: ""
+        val simpleName = it.simpleName ?: ""
+        val importInfo = ImportInfo(canonicalName, simpleName)
+        allClasses.add(importInfo)
+      }
+      if (kotlinClass.visibility == KVisibility.PUBLIC) {
+        val canonicalName = kotlinClass.qualifiedName ?: ""
+        val simpleName = kotlinClass.simpleName ?: ""
+        val importInfo = ImportInfo(canonicalName, simpleName)
+        allClasses.add(importInfo)
+      }
+    } catch (exception: UnsupportedOperationException) {
+      // throw kotlinClass.nestedClasses
+      return allClassesFromJavaClass(clazz)
+    } catch (error: AssertionError) {
+      /* In Tests:
+      1) kotlinx.coroutines.flow.internal.ChannelFlowKt$withContextUndispatched$$inlined
+        $suspendCoroutineUninterceptedOrReturn$lambda$1
+      2) kotlin.coroutines.experimental.intrinsics.IntrinsicsKt__IntrinsicsJvmKt
+        $createCoroutineUnchecked$$inlined$buildContinuationByInvokeCall$IntrinsicsKt__IntrinsicsJvmKt$1
+      3) kotlin.coroutines.experimental.intrinsics.IntrinsicsKt__IntrinsicsJvmKt$createCoroutineUnchecked
+        $$inlined$buildContinuationByInvokeCall$IntrinsicsKt__IntrinsicsJvmKt$2
+      */
+      return allClassesFromJavaClass(clazz)
+    } catch (error: IncompatibleClassChangeError) {
+      /* In Application:
+      1) kotlinx.coroutines.CoroutineDispatcher$Key
+      2) kotlinx.coroutines.channels.ConflatedChannel$Companion
+      3) kotlinx.coroutines.ExecutorCoroutineDispatcher$Key
+      */
+      return allClassesFromJavaClass(clazz)
+    } catch (error: InternalError) {
+      /* In Application:
+      1-53) kotlinx.coroutines.flow.FlowKt__*
+      54-59) kotlinx.coroutines.channels.*
+       */
+      return allClassesFromJavaClass(clazz)
+    } catch (exception: NoSuchElementException) {
+      /* In Application:
+      1) kotlinx.coroutines.flow.internal.ChannelFlowKt$withContextUndispatched$$inlined
+        $suspendCoroutineUninterceptedOrReturn$lambda$1
+      2) kotlin.coroutines.experimental.intrinsics.IntrinsicsKt__IntrinsicsJvmKt$createCoroutineUnchecked
+        $$inlined$buildContinuationByInvokeCall$IntrinsicsKt__IntrinsicsJvmKt$1
+      3) kotlin.coroutines.experimental.intrinsics.IntrinsicsKt__IntrinsicsJvmKt$createCoroutineUnchecked
+        $$inlined$buildContinuationByInvokeCall$IntrinsicsKt__IntrinsicsJvmKt$2
+      */
+      return allClassesFromJavaClass(clazz)
+    }
+    return allClasses
+  }
+
+  private fun getVariantsForZip(file: File): List<ImportInfo> {
+    val jarFile = JarFile(file)
+    val allSuggests = hashSetOf<ImportInfo>()
+    jarFile.entries().toList().map { entry ->
+      if (!entry.isDirectory
+        && entry.name.endsWith(".class")
+      ) {
+        val name = entry.name.removeSuffix(".class")
+        val fullName = name.replace("/", ".")
+        if (fullName != MODULE_INFO_NAME) {
+          try {
+            val clazz = ClassLoader.getSystemClassLoader().loadClass(fullName)
+            if (clazz.isKotlinClass()) {
+              allSuggests.addAll(allClassesFromKotlinClass(clazz))
+            } else {
+              allSuggests.addAll(allClassesFromJavaClass(clazz))
+            }
+            allSuggests.addAll(allFunctionsFromClass(clazz))
+          } catch (error: VerifyError) {
+          /* In Application:
+            1) kotlinx.coroutines.EventLoopImplBase$DelayedTaskQueue
+            2) kotlinx.coroutines.flow.internal.FlowProduceCoroutine
+            */
+          } catch (error: NoClassDefFoundError) {
+            /* In Application:
+            1) kotlinx.coroutines.flow.FlowKt__MergeKt$flatMapLatest$1
+            2) kotlinx.coroutines.flow.FlowKt__MergeKt$mapLatest$1
+            3) kotlinx.coroutines.flow.FlowKt__MergeKt$flattenConcat$$inlined$unsafeFlow$1
+            4) kotlinx.coroutines.flow.FlowKt__MergeKt$flatMapConcat$$inlined$map$1
+            5) kotlinx.coroutines.flow.FlowKt__MergeKt$flatMapMerge$$inlined$map$1
+             */
+          }
+        }
+      }
+    }
+    return allSuggests.toList()
+  }
+
+  private fun allFunctionsFromClass(clazz: Class<*>): HashSet<ImportInfo> {
+    val allClasses = hashSetOf<ImportInfo>()
+    clazz.declaredMethods.map { method ->
+      val importInfo = importInfoFromFunction(method, clazz)
+      if (importInfo != null) allClasses.add(importInfo)
+    }
+    return allClasses
+  }
+
+  private fun importInfoFromFunction(method: Method, clazz: Class<*>): ImportInfo? {
+    var kotlinFunction : KFunction<*>? = null
+    try {
+      kotlinFunction = method.kotlinFunction
+    } catch (exception: NoSuchElementException) {
+    } catch (exception: UnsupportedOperationException) {
+    } catch (error: KotlinReflectionInternalError) {
+    } catch (error: AssertionError) {
+    } catch (error: InternalError) {
+    } catch (error: IncompatibleClassChangeError) {
+    }
+    return if (kotlinFunction != null
+      && kotlinFunction.visibility == KVisibility.PUBLIC) {
+      importInfoByMethodAndParent(
+        kotlinFunction.name,
+        kotlinFunction.parameters.map { it.type }.joinToString(),
+        clazz)
+    } else importInfoFromJavaMethod(method, clazz)
+  }
+
+  private fun importInfoFromJavaMethod(method: Method, clazz: Class<*>): ImportInfo? =
+    if (Modifier.isPublic(method.modifiers) &&
+      Modifier.isStatic(method.modifiers))
+      importInfoByMethodAndParent(method.name, method.parameters.map { it.type.name }.joinToString(), clazz)
+    else null
+
+  private fun importInfoByMethodAndParent(methodName: String, parametersString: String, parent: Class<*>): ImportInfo {
+    val shortName = methodName.split("$").first()
+    val className = "$shortName($parametersString)"
+    val fullName = "${parent.`package`.name}.$shortName"
+    return ImportInfo(fullName, className)
   }
 
   private fun getAllVariants(): List<ImportInfo> {
@@ -423,7 +464,6 @@ class SuggestionProvider(
   }
 
   fun getClassByPrefix(prefix: String): List<ImportInfo> {
-    File("output").writeText("")
     return ALL_INDEXES.filter { variant ->
       variant.className.startsWith(prefix)
     }.sortedBy { it.className.length }
