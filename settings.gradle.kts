e1548b4ade69165586c0a3dccb2647de8c500a5e
anastasiiaSpaseeva <57627321+anastasiiaSpaseeva@users.noreply.github.com>
2021-02-04 13:30:05 +0300

    chore(release): support 1.4.30 and migrate to new IDE repository (#243)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,9 +4,5 @@ include(":indexation")
 include(":common")
 
 pluginManagement {
-  repositories {
-    mavenCentral()
-    gradlePluginPortal()
-    maven("https://cache-redirector.jetbrains.com/kotlin.bintray.com/kotlin-plugin")
-  }
+
 }
