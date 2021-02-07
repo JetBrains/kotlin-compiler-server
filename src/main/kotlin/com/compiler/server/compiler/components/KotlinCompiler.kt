1a9a304465fb9be63f0cbb853caae38ce6183b78
Andrei Efimov <andrey.efimov@t-systems.com>
2021-02-07 22:45:33 +0300

    fix: windows host process execution

--- src/main/kotlin/com/compiler/server/compiler/components/KotlinCompiler.kt
+++ src/main/kotlin/com/compiler/server/compiler/components/KotlinCompiler.kt
@@ -97,8 +97,8 @@ class KotlinCompiler(
     val sessionId = UUID.randomUUID().toString().replace("-", "")
     val outputDir = Paths.get(dir, "tmp", sessionId)
     val policy = policyFile.readText()
-      .replace("%%GENERATED%%", outputDir.toString())
-      .replace("%%LIB_DIR%%", libDir)
+      .replace("%%GENERATED%%", outputDir.toString().replace('\\','/'))
+      .replace("%%LIB_DIR%%", libDir.replace('\\','/'))
     outputDir.resolve(policyFile.name).apply { parent.toFile().mkdirs() }.toFile().writeText(policy)
     return OutputDirectory(outputDir, compiled.files.map { (name, bytes) ->
       outputDir.resolve(name).let { path ->
@@ -128,8 +128,9 @@ class KotlinCompiler(
     outputDirectory: OutputDirectory,
     args: List<String>
   ): List<String> {
+    val pathSeparator = System.getProperty("path.separator") ?: ":"
     val classPaths = (kotlinEnvironment.classpath.map { it.absolutePath } + outputDirectory.path.toAbsolutePath().toString())
-      .joinToString(":")
+      .joinToString(pathSeparator)
     val policy = outputDirectory.path.resolve(policyFile.name).toAbsolutePath()
     return CommandLineArgument(
       classPaths = classPaths,
