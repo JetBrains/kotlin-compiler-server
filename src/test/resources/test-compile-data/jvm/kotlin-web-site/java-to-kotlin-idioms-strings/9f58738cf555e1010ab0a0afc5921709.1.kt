afb88eed82e4493ba4434a822a95ce7d1f98949d
Alexander Prendota <Prendota@mail.ru>
2021-06-04 17:29:25 +0300

    Revert "test(samples): add new samples (#314)" (#316)
    This reverts commit 9d83ba9834b34a6a16b7782b953f6edcd67d9e5c.

--- src/test/resources/test-compile-data/jvm/kotlin-web-site/java-to-kotlin-idioms-strings/9f58738cf555e1010ab0a0afc5921709.1.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-//sampleStart
-    // Kotlin
-    val name = "Joe"
-    println("Hello, $name")
-    println("Your name is ${name.length} characters long")
-//sampleEnd
-}
--- src/test/resources/test-compile-data/jvm/kotlin-web-site/java-to-kotlin-idioms-strings/9f58738cf555e1010ab0a0afc5921709.9.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-//sampleStart
-    // Kotlin
-    val input = "To be, or not to be, that is the question."
-    val question = input.substringAfterLast(",")
-    println(question)
-//sampleEnd
-}
