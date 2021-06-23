f2c819fcfd495464da2cd14ec8a01835521837b6
Margarita Bobova <Margarita.Bobova@jetbrains.com>
2021-06-23 15:59:49 +0700

    add 1.5.20-282 version

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,8 +45,9 @@ val copyJSDependencies by tasks.creating(Copy::class) {
 plugins {
     id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.10"
-    kotlin("plugin.spring") version "1.5.10"
+    kotlin("jvm") version "1.5.20-282"
+    kotlin("plugin.spring") version "1.5.20-282"
+
 }
 
 allprojects {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
+    }
+    plugins{
+        kotlin("jvm") version "1.5.20-282"
+        kotlin("plugin.spring") version "1.5.20-282"
+    }
+}
+
 rootProject.name = "kotlin-compiler-server"
 include(":executors")
 include(":indexation")
