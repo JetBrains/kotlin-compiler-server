a30582ebc115f66711e27bfad3b65f6dea7986a5
Margarita Bobova <32216159+woainikk@users.noreply.github.com>
2021-08-24 16:43:36 +0300

    release: 1.5.30 (#343)
    * chore(release): add kotlin 1.5.30
    
    * chore(release): turn off transitive dependency for ide artifacts
    
    * chore(release): fix js reference

--- Dockerfile
+++ Dockerfile
@@ -12,8 +12,8 @@ FROM openjdk:8-jdk-alpine
 RUN mkdir /kotlin-compiler-server
 WORKDIR /kotlin-compiler-server
 
-ENV KOTLIN_LIB=1.5.21
-ENV KOTLIN_LIB_JS=1.5.21-js
+ENV KOTLIN_LIB=1.5.30
+ENV KOTLIN_LIB_JS=1.5.30-js
 
 COPY --from=build /build/libs/BOOT-INF/lib /kotlin-compiler-server/lib
 COPY --from=build /build/libs/META-INF /kotlin-compiler-server/META-INF
--- build.gradle.kts
+++ build.gradle.kts
@@ -45,8 +45,8 @@ val copyJSDependencies by tasks.creating(Copy::class) {
 plugins {
     id("org.springframework.boot") version "2.5.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.21"
-    kotlin("plugin.spring") version "1.5.21"
+    kotlin("jvm") version "1.5.30"
+    kotlin("plugin.spring") version "1.5.30"
 }
 
 allprojects {
@@ -56,6 +56,7 @@ allprojects {
         maven("https://cache-redirector.jetbrains.com/jetbrains.bintray.com/intellij-third-party-dependencies")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
         maven("https://www.myget.org/F/rd-snapshots/maven/")
+        maven("https://kotlin.jetbrains.space/p/kotlin/packages/maven/kotlin-ide")
     }
     afterEvaluate {
         dependencies {
@@ -97,7 +98,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-compiler:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-script-runtime:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-js:$kotlinVersion")
-    implementation("org.jetbrains.kotlin:kotlin-compiler-for-ide:$kotlinIdeVersion")
+    implementation("org.jetbrains.kotlin:kotlin-compiler-for-ide:$kotlinIdeVersion"){
+        isTransitive = false
+    }
     implementation("org.jetbrains.kotlin:common:202-$kotlinIdeVersion-IJ8194.7")
     implementation("org.jetbrains.kotlin:core:202-$kotlinIdeVersion-IJ8194.7")
     implementation(project(":executors", configuration = "default"))
