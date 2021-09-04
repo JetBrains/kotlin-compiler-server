5a98849ee58f4992eaa75bb3424205ccedd30135
Prendota <Prendota@mail.ru>
2021-09-04 22:04:24 +0300

    chore(logger): init logger helper

---
+++ src/main/kotlin/com/compiler/server/utils/LoggerHelper.kt
@@ -0,0 +1,29 @@
+package com.compiler.server.utils
+
+import com.compiler.server.model.ExecutionResult
+import com.compiler.server.model.ProjectType
+import net.logstash.logback.marker.Markers
+import org.slf4j.LoggerFactory
+
+object LoggerHelper {
+  private val log = LoggerFactory.getLogger(LoggerHelper::class.java)
+
+  fun logUnsuccessfulExecutionResult(executionResult: ExecutionResult, type: ProjectType, version: String) {
+    if (executionResult.isUnsuccessful().not()) return
+    log.info(
+      Markers.appendFields(
+        UnsuccessfulExecutionDetails(
+          executionResult.errorsMessages(),
+          type.toString(),
+          version
+        )
+      ), "Execution is unsuccessful."
+    )
+  }
+
+  private data class UnsuccessfulExecutionDetails(
+    val executionErrors: List<String>,
+    val confType: String,
+    val kotlinVersion: String
+  )
+}
