1f058e8b73d5d86b744ab1690d2c818b1a3067e4
nikpachoo <nikpachoo@gmail.com>
2021-09-04 22:06:10 +0300

    chore(logger): init

--- src/main/kotlin/com/compiler/server/model/ExecutionResult.kt
+++ src/main/kotlin/com/compiler/server/model/ExecutionResult.kt
@@ -14,6 +14,19 @@ open class ExecutionResult(
   fun addWarnings(warnings: Map<String, List<ErrorDescriptor>>) {
     errors = warnings
   }
+
+  fun isUnsuccessful() =
+    textWithError() || errors.any { (_, value) -> value.any { it.severity == ProjectSeveriry.ERROR } }
+
+  fun errorsMessages(): List<String> {
+    val compilationErrors =
+      errors.flatMap { it.value }.filter { it.severity == ProjectSeveriry.ERROR }.map { it.message }
+
+    return if (textWithError()) compilationErrors + text
+    else compilationErrors
+  }
+
+  private fun textWithError() = text.startsWith(ERROR_STREAM_START)
 }
 
 data class TranslationJSResult(
