a6c9db522f265e385f154085521a33fafa8fcb49
Margarita Bobova <Margarita.Bobova@jetbrains.com>
2021-09-19 10:57:31 +0300

    chore(release): add 1.5.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,8 +45,8 @@ val copyJSDependencies by tasks.creating(Copy::class) {
 plugins {
     id("org.springframework.boot") version "2.5.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.30"
-    kotlin("plugin.spring") version "1.5.30"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31"
 }
 
 allprojects {
