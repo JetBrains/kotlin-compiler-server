67a0e2f5e577cf65ad9e7fc1593b13e9afc2a21b
Margarita Bobova <Margarita.Bobova@jetbrains.com>
2022-05-12 14:24:53 +0200

    chore(release): add kotlin 1.7.0-Beta

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -9,14 +9,3 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-compiler:$kotlinVersion")
     implementation("org.jetbrains.kotlin:common:213-$kotlinIdeVersion-IJ5744.223")
 }
-
-allprojects {
-    repositories {
-        mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide")
-        //maven("https://cache-redirector.jetbrains.com/jetbrains.bintray.com/intellij-third-party-dependencies")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
-        maven("https://kotlin.jetbrains.space/p/kotlin/packages/maven/kotlin-ide")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
-    }
-}
