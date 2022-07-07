5c51eb1cc4b42d61bf24594ed8f2a6d3119dd2e9
Margarita Bobova <Margarita.Bobova@jetbrains.com>
2022-07-07 15:50:39 +0200

    chore(release): add kotlin 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ val copyJSDependencies by tasks.creating(Copy::class) {
 plugins {
     id("org.springframework.boot") version "2.5.6"
     id("io.spring.dependency-management") version "1.0.12.RELEASE"
-    kotlin("jvm") version "1.7.0"
-    kotlin("plugin.spring") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.10"
 }
 
 allprojects {
