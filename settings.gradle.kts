637c36dd768292377e99d8d6c909a2aa30121e6d
Margarita Bobova <Margarita.Bobova@jetbrains.com>
2022-07-29 19:53:14 +0200

    kotlin 1.7.20-Beta-79

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,10 @@ rootProject.name = "kotlin-compiler-server"
 include(":executors")
 include(":indexation")
 include(":common")
+
+pluginManagement {
+  repositories {
+    maven("https://cache-redirector.jetbrains.com/maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+    mavenCentral()
+  }
+}
