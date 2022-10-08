f458d72eb954ba5a1c6552a0fe0fc7afedc93bc6
Prendota <Prendota@mail.ru>
2022-10-09 00:06:07 +0200

    chore(log): make log component
    use json logger with prod profile

--- src/main/kotlin/com/compiler/server/compiler/components/LoggerDetailsStreamer.kt
+++ src/main/kotlin/com/compiler/server/compiler/components/LoggerDetailsStreamer.kt
@@ -1,12 +1,16 @@
-package com.compiler.server.utils
+package com.compiler.server.compiler.components
 
 import com.compiler.server.model.ExecutionResult
 import com.compiler.server.model.ProjectType
 import net.logstash.logback.marker.Markers
 import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
 
-object LoggerHelper {
-  private val log = LoggerFactory.getLogger(LoggerHelper::class.java)
+@Component
+@Profile("prod")
+class LoggerDetailsStreamer {
+  private val log = LoggerFactory.getLogger(LoggerDetailsStreamer::class.java)
 
   fun logExecutionResult(executionResult: ExecutionResult, type: ProjectType, version: String) {
     log.info(
@@ -16,7 +20,8 @@ object LoggerHelper {
           confType = type.toString(),
           version = version
         )
-      ), "Code execution is complete."
+      ),
+      "Code execution is complete."
     )
   }
 
