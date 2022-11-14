0820b8203ebf0dac327a85844146e4cf751537a1
Margarita Bobova <Margarita.Bobova@jetbrains.com>
2022-11-14 21:47:06 +0100

    chore(release): add kotlin 1.8.0-Beta

--- src/test/resources/test-compile-data/jvm/kotlin-web-site/whatsnew15/f581dd74809f95d6448c98641a1ef376.2.kt
+++ src/test/resources/test-compile-data/jvm/kotlin-web-site/whatsnew15/f581dd74809f95d6448c98641a1ef376.2.kt
@@ -1,10 +1,11 @@
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
 fun main() {
 //sampleStart
-    val duration = Duration.milliseconds(120000)
+    val duration = 120000.milliseconds
     println("There are ${duration.inWholeSeconds} seconds in ${duration.inWholeMinutes} minutes")
 //sampleEnd
 }
