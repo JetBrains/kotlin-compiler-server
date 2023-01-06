30f11c3c39e14f811e566a67916f1a229438a49b
Margarita Bobova <margarita.bobova@jetbrains.com>
2023-01-06 13:24:18 +0100

    Use 222 platform kotlin ide artifacts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -8,4 +8,5 @@ plugins {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-compiler:$kotlinVersion")
     implementation("org.jetbrains.kotlin:common:222-$kotlinIdeVersion-IJ4167.29")
+    implementation("org.jetbrains.kotlin:base-fe10-analysis:222-$kotlinIdeVersion-IJ4167.29")
 }
