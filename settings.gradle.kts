c8781c1ef899893847a77d3f076b62da4f908972
Margarita Bobova <margarita.bobova@jetbrains.com>
2023-01-31 11:07:56 +0100

    Kotlin 222-1.8.20-Beta-90

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,11 @@ rootProject.name = "kotlin-compiler-server"
 include(":executors")
 include(":indexation")
 include(":common")
+
+pluginManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
