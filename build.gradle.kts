a677cee67d6dba16c3db91cbd7cb67e8c05ec46b
Margarita Bobova <margarita.bobova@jetbrains.com>
2023-01-31 11:07:56 +0100

    chore(release): add kotlin 1.8.20-Beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ val copyJSDependencies by tasks.creating(Copy::class) {
 plugins {
     id("org.springframework.boot") version "2.7.8"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.10"
-    kotlin("plugin.spring") version "1.8.10"
+    kotlin("jvm") version "1.8.20-Beta"
+    kotlin("plugin.spring") version "1.8.20-Beta"
 }
 
 allprojects {
