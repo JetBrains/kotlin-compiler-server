c5841352f811e18e23dcf149ee6ceac7abcb95ce
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2023-03-14 12:09:26 +0100

    Revert "feat: ir ic (#604)"
    This reverts commit dc4983e1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,3 @@ rootProject.name = "kotlin-compiler-server"
 include(":executors")
 include(":indexation")
 include(":common")
-include(":cache-preparator")
--- src/main/kotlin/com/compiler/server/compiler/components/KotlinEnvironment.kt
+++ src/main/kotlin/com/compiler/server/compiler/components/KotlinEnvironment.kt
@@ -1,16 +1,12 @@
 package com.compiler.server.compiler.components
 
-import com.compiler.server.model.bean.CachesFile
 import com.compiler.server.model.bean.LibrariesFile
 import component.KotlinEnvironment
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class KotlinEnvironmentConfiguration(
-  val librariesFile: LibrariesFile,
-  val cachesFiles: CachesFile
-) {
+class KotlinEnvironmentConfiguration(val librariesFile: LibrariesFile) {
   @Bean
   fun kotlinEnvironment(): KotlinEnvironment {
     val classPath =
@@ -21,6 +17,6 @@ class KotlinEnvironmentConfiguration(
         }
 
     val additionalJsClasspath = listOfNotNull(librariesFile.js)
-    return KotlinEnvironment(classPath, additionalJsClasspath, cachesFiles.js)
+    return KotlinEnvironment(classPath, additionalJsClasspath)
   }
 }
