c97fc8b2d99526a94bbb7383b058b7257a392208
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2023-04-13 11:55:33 +0200

    Fix rebase problem with duplicating plugin management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,12 @@
-pluginManagement {
-  repositories {
-    gradlePluginPortal()
-    mavenCentral()
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
-  }
-}
 rootProject.name = "kotlin-compiler-server"
 include(":executors")
 include(":indexation")
 include(":common")
 
-pluginManagement{
-    repositories{
+pluginManagement {
+    repositories {
         gradlePluginPortal()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
     }
 }
