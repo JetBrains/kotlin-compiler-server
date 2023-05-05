fd2e71e300a32e11243df23696c5f0b22317607e
Nikolay Pachkov <nikolay.pachkov@jetbrains.com>
2023-05-05 13:55:23 +0200

    ktl-1017 chore: simplify getting the kotlin version in the docker build script

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-kotlin_version=$(grep "systemProp.kotlinVersion" gradle.properties | cut -d'=' -f2)
+kotlin_version=$(awk '{ if ($1=="systemProp.kotlinVersion") { print $2; } }' FS='=' ./gradle.properties)
 
 docker build . --file Dockerfile --tag my-image-name:$(date +%s) --build-arg KOTLIN_VERSION=$kotlin_version
