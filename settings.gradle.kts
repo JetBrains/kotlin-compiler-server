abb5962a8b7be767773028c30820e658601d4d6c
Margarita Bobova <margarita.bobova@jetbrains.com>
2023-05-10 16:45:08 +0200

    Kotlin 1.9.0-Beta-61

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ include(":common")
 
 pluginManagement{
     repositories{
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         gradlePluginPortal()
         mavenCentral()
     }
