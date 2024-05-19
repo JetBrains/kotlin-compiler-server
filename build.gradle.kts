38b0570f4f7bb4d8b6a8cc9cca8db0dfbf0ec44a
zoobestik <kb.chernenko@gmail.com>
2024-05-19 20:59:36 +0200

    chore: use jetty instead of tomcat

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,8 +62,12 @@ val resourceDependency: Configuration by configurations.creating {
 dependencies {
     annotationProcessor("org.springframework:spring-context-indexer")
     implementation("com.google.code.gson:gson")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.amazonaws.serverless:aws-serverless-java-container-springboot2:1.9.3")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude(group="org.springframework.boot", module="spring-boot-starter-tomcat")
+        exclude("tomcat-embed-*")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-jetty")
+    implementation("com.amazonaws.serverless:aws-serverless-java-container-springboot2:1.9.4")
     implementation("junit:junit:4.13.2")
     implementation("net.logstash.logback:logstash-logback-encoder:7.3")
     implementation("org.jetbrains.intellij.deps:trove4j:1.0.20221201")
