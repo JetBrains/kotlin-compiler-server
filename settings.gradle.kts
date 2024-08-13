92d93c1a6925bc21f3ecf7218dc0fc641760e42d
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2024-08-13 12:17:37 +0200

    Extracted common Gradle settings logic to the settings plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,17 @@
 rootProject.name = "kotlin-compiler-server"
-include(":executors")
-include(":indexation")
-include(":common")
-include(":dependencies")
-
 pluginManagement {
+    includeBuild("build-settings-logic")
     repositories {
         gradlePluginPortal()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
     }
 }
-
-dependencyResolutionManagement {
-    versionCatalogs {
-        val kotlinVersion = providers.gradleProperty("kotlinVersion")
-        if (kotlinVersion.isPresent) {
-            create("libs") {
-                version("kotlin", kotlinVersion.get())
-            }
-        }
-    }
+plugins {
+    id("kotlin-compiler-server-version-catalog")
 }
+
+include(":executors")
+include(":indexation")
+include(":common")
+include(":dependencies")
