d89fd04a76871645ee9a6968648538eeef405e15
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2024-08-13 12:17:37 +0200

    Extracted common Gradle settings logic to the settings plugin

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,13 +1,19 @@
+pluginManagement {
+    includeBuild("../build-settings-logic")
+}
+
 dependencyResolutionManagement {
+    // For buildSrc we need to declare a custom path to the toml file with versions' catalog.
+    // But for a root project we can't set `from` inside `versionCatalogs` catalog block for the default `libs` catalog.
+    // (see https://github.com/gradle/gradle/issues/21328)
+    // That is why it is not fully moved to the dependencyResolutionManagement block in the settings convention plugin.
     versionCatalogs {
-        create("libs") {
+        getByName("libs") {
             from(files("../gradle/libs.versions.toml"))
         }
-        val kotlinVersion = providers.gradleProperty("kotlinVersion")
-        if (kotlinVersion.isPresent) {
-            create("libs") {
-                version("kotlin", kotlinVersion.get())
-            }
-        }
     }
+}
+
+plugins {
+    id("kotlin-compiler-server-version-catalog")
 }
