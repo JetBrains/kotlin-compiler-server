732e89eddc8ed399f3b8748a2c1427bc749e3f49
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2024-10-07 12:31:20 +0200

    fixup! Rewrote expected output for tests

--- src/test/resources/test-compile-data/jvm/kotlin-web-site/object-declarations/8a72411880f54ee0a305b57ed6c6901d.2.kt
+++ src/test/resources/test-compile-data/jvm/kotlin-web-site/object-declarations/8a72411880f54ee0a305b57ed6c6901d.2.kt
@@ -1,6 +1,12 @@
-object MyObject
+object MyObject {
+    override fun toString(): String {
+        val superString = super.toString()
+        // MyObject@hashcode
+        return superString.substringBefore('@')
+    }
+}
 
 fun main() {
-    println(MyObject) 
-    // MyObject@hashcode
+    println(MyObject)
+    // MyObject
 }
