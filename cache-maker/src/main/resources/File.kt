4db41aa788090925a5911074af2e968eba8c01bb
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2024-12-05 00:07:54 +0100

    Fix sample to be compatible with example in playground

--- cache-maker/src/main/resources/File.kt
+++ cache-maker/src/main/resources/File.kt
@@ -1,22 +1,24 @@
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.CanvasBasedWindow
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.window.ComposeViewport
-import kotlinx.browser.document
 
 //sampleStart
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    ComposeViewport(document.body!!) {
-        App()
-    }
+    CanvasBasedWindow { App() }
 }
 
 @Composable
