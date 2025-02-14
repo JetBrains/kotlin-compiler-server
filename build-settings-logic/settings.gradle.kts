0266dd3c2b33c5d96a3aed13f19e516b04769cac
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2025-02-14 13:52:41 +0100

    Extracted repositories
    ^KTL-1724

--- build-settings-logic/settings.gradle.kts
+++ build-settings-logic/settings.gradle.kts
@@ -1,14 +1,14 @@
 rootProject.name = "build-settings-logic"
 
-dependencyResolutionManagement {
+apply(from = "src/main/kotlin/kotlin-compiler-server-version-catalog.settings.gradle.kts")
 
-    @Suppress("UnstableApiUsage")
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
+dependencyResolutionManagement {
+    // For buildSrc we need to declare a custom path to the toml file with versions' catalog.
+    // But for a root project we can't set `from` inside `versionCatalogs` catalog block for the default `libs` catalog.
+    // (see https://github.com/gradle/gradle/issues/21328)
+    // That is why it is not fully moved to the dependencyResolutionManagement block in the settings convention plugin.
     versionCatalogs {
-        create("libs") {
+        getByName("libs") {
             from(files("../gradle/libs.versions.toml"))
         }
     }
