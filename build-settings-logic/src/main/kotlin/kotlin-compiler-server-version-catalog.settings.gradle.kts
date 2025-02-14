a02e6e4c63a2bf5394a8c8297ac8e85fef7945f8
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2025-02-14 13:52:41 +0100

    Extracted repositories
    ^KTL-1724

--- build-settings-logic/src/main/kotlin/kotlin-compiler-server-version-catalog.settings.gradle.kts
+++ build-settings-logic/src/main/kotlin/kotlin-compiler-server-version-catalog.settings.gradle.kts
@@ -1,4 +1,48 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+
+        val additionalRepositoryProperty = providers.gradleProperty("kotlin_repo_url")
+        if (additionalRepositoryProperty.isPresent) {
+            maven(additionalRepositoryProperty.get()) {
+                name = "KotlinDevRepo"
+            }
+            logger.info("A custom Kotlin repository ${additionalRepositoryProperty.get()} was added")
+        }
+
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+    }
+}
+
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+
+        val additionalRepositoryProperty = providers.gradleProperty("kotlin_repo_url")
+        if (additionalRepositoryProperty.isPresent) {
+            maven(additionalRepositoryProperty.get()) {
+                name = "KotlinDevRepo"
+            }
+            logger.info("A custom Kotlin repository ${additionalRepositoryProperty.get()} was added")
+        }
+
+        maven("https://repo.spring.io/snapshot")
+        maven("https://repo.spring.io/milestone")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        maven("https://cache-redirector.jetbrains.com/jetbrains.bintray.com/intellij-third-party-dependencies")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
+        maven("https://www.myget.org/F/rd-snapshots/maven/")
+        maven("https://kotlin.jetbrains.space/p/kotlin/packages/maven/kotlin-ide")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
     versionCatalogs {
         register("libs").configure {
             val kotlinVersion = providers.gradleProperty("kotlin_version")
