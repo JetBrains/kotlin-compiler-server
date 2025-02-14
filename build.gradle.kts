267e710705be69093a3ed6e2a7263f54958fe192
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2025-02-14 13:52:41 +0100

    Extracted repositories
    ^KTL-1724

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,21 +21,6 @@ plugins {
 apply<NodeJsRootPlugin>()
 
 allprojects {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        maven("https://repo.spring.io/snapshot")
-        maven("https://repo.spring.io/milestone")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
-        maven("https://cache-redirector.jetbrains.com/jetbrains.bintray.com/intellij-third-party-dependencies")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
-        maven("https://www.myget.org/F/rd-snapshots/maven/")
-        maven("https://kotlin.jetbrains.space/p/kotlin/packages/maven/kotlin-ide")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
     afterEvaluate {
         dependencies {
             dependencies {
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-repositories {
-    mavenCentral()
-}
-
 // workaround to pass libs into conventions (see https://github.com/gradle/gradle/issues/15383)
 dependencies {
     implementation(libs.kotlin.gradlePlugin)
