8166700f54537c72b02bffe318aa174c3a2800ca
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2025-03-27 08:45:48 +0100

    Added develocity access key for Gradle build scans
    #TI-1997

--- docker-image-build.sh
+++ docker-image-build.sh
@@ -4,4 +4,4 @@ kotlinVersion=$(awk '{ if ($1 == "kotlin") { gsub(/"/, "", $2); print $2; } }' F
 
 echo "Kotlin Version for the docker: $kotlinVersion"
 
-docker build . --file Dockerfile --tag my-image-name:$(date +%s) --build-arg KOTLIN_VERSION=$kotlinVersion
+docker build . --file Dockerfile --tag my-image-name:$(date +%s) --build-arg KOTLIN_VERSION=$kotlinVersion --build-arg DEVELOCITY_ACCESS_KEY=$DEVELOCITY_ACCESS_KEY
