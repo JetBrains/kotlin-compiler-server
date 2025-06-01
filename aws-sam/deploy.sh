120d9d3895f10de7f6b222ec15381a87efb3f17f
zoobestik <kb.chernenko@gmail.com>
2025-06-01 21:40:58 +0200

    chore; upgrade version of compose

--- aws-sam/deploy.sh
+++ aws-sam/deploy.sh
@@ -5,5 +5,5 @@ cp -f ../build/distributions/kotlin-compiler-server-*-SNAPSHOT.zip ./kotlin-comp
 sam build
 sam deploy
 
-aws s3 sync ./build/compose-wasm-resources/ s3://kotlin-compose-stage-composestaticbucket-w4wbe7v2wt1h/api/resource/ --delete
+aws s3 sync ./build/compose-wasm-resources/ s3://kotlin-compose-stage-composestaticbucket-w4wbe7v2wt1h/api/resource/
 aws cloudfront create-invalidation --distribution-id E3LSIHO1W3290M --paths "/*"
