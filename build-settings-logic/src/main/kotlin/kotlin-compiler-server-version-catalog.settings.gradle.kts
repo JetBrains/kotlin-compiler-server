4c0f58afe9f7cce711ca5603cc1d458cbc9ea25e
zoobestik <kb.chernenko@gmail.com>
2025-06-02 15:59:41 +0200

    Fix maven repositories after space shutdown

--- build-settings-logic/src/main/kotlin/kotlin-compiler-server-version-catalog.settings.gradle.kts
+++ build-settings-logic/src/main/kotlin/kotlin-compiler-server-version-catalog.settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
         }
 
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        maven("https://redirector.kotlinlang.org/maven/dev")
     }
 }
 
@@ -34,12 +34,12 @@ dependencyResolutionManagement {
 
         maven("https://repo.spring.io/snapshot")
         maven("https://repo.spring.io/milestone")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        maven("https://redirector.kotlinlang.org/maven/kotlin-ide")
+        maven("https://redirector.kotlinlang.org/maven/dev")
         maven("https://cache-redirector.jetbrains.com/jetbrains.bintray.com/intellij-third-party-dependencies")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-ide-plugin-dependencies")
         maven("https://www.myget.org/F/rd-snapshots/maven/")
-        maven("https://kotlin.jetbrains.space/p/kotlin/packages/maven/kotlin-ide")
+        maven("https://redirector.kotlinlang.org/maven/kotlin-ide")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
