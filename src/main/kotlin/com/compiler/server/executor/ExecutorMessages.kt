85c80df13990d47f21888163c3adffea7cffacb8
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2025-08-07 09:57:29 +0200

    KTL-1220: Removed indexation module and kotlin idea dependencies

--- src/main/kotlin/com/compiler/server/executor/ExecutorMessages.kt
+++ src/main/kotlin/com/compiler/server/executor/ExecutorMessages.kt
@@ -3,5 +3,4 @@ package com.compiler.server.executor
 object ExecutorMessages {
   const val TIMEOUT_MESSAGE = "Evaluation stopped while it's taking too long️"
   const val TOO_LONG_OUTPUT_MESSAGE = "Evaluation stopped while log size exceeded max size"
-  const val NO_COMPILERS_FILE_FOUND = "No compilation files found!"
 }
