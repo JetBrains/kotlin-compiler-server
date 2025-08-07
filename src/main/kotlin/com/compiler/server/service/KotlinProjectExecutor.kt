0744de2c86b95433e3091413801f44466d302262
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2025-08-07 09:57:29 +0200

    KTL-1220: Removed indexation module and kotlin idea dependencies

--- src/main/kotlin/com/compiler/server/service/KotlinProjectExecutor.kt
+++ src/main/kotlin/com/compiler/server/service/KotlinProjectExecutor.kt
@@ -1,128 +1,111 @@
 package com.compiler.server.service
 
-import com.compiler.server.compiler.KotlinFile
 import com.compiler.server.compiler.components.*
 import com.compiler.server.model.*
 import com.compiler.server.model.bean.VersionInfo
 import component.KotlinEnvironment
 import model.Completion
-import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
-import org.jetbrains.kotlin.psi.KtFile
+import org.junit.Ignore
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class KotlinProjectExecutor(
-  private val kotlinCompiler: KotlinCompiler,
-  private val completionProvider: CompletionProvider,
-  private val version: VersionInfo,
-  private val kotlinToJSTranslator: KotlinToJSTranslator,
-  private val kotlinEnvironment: KotlinEnvironment,
-  private val loggerDetailsStreamer: LoggerDetailsStreamer? = null,
+    private val kotlinCompiler: KotlinCompiler,
+    private val environment: KotlinEnvironment,
+    private val version: VersionInfo,
+    private val kotlinToJSTranslator: KotlinToJSTranslator,
+    private val loggerDetailsStreamer: LoggerDetailsStreamer? = null,
 ) {
 
-  private val log = LoggerFactory.getLogger(KotlinProjectExecutor::class.java)
+    private val log = LoggerFactory.getLogger(KotlinProjectExecutor::class.java)
 
-  fun run(project: Project, addByteCode: Boolean): ExecutionResult {
-    return kotlinEnvironment.environment { environment ->
-      val files = getFilesFrom(project, environment).map { it.kotlinFile }
-      kotlinCompiler.run(files, addByteCode, project.args)
-    }.also { logExecutionResult(project, it) }
-  }
-
-  fun test(project: Project, addByteCode: Boolean): ExecutionResult {
-    return kotlinEnvironment.environment { environment ->
-      val files = getFilesFrom(project, environment).map { it.kotlinFile }
-      kotlinCompiler.test(files, addByteCode)
-    }.also { logExecutionResult(project, it) }
-  }
-
-  fun convertToJsIr(project: Project): TranslationJSResult {
-    return convertJsWithConverter(project, kotlinToJSTranslator::doTranslateWithIr)
-  }
-
-  fun compileToJvm(project: Project): CompilationResult<KotlinCompiler.JvmClasses> {
-    val files = kotlinEnvironment.environment { environment ->
-      getFilesFrom(project, environment).map { it.kotlinFile }
+    fun run(project: Project, addByteCode: Boolean): ExecutionResult {
+        return environment.synchronize { kotlinCompiler.run(project.files, addByteCode, project.args) }
+            .also { logExecutionResult(project, it) }
     }
-    return kotlinCompiler.compile(files)
-  }
 
-  fun convertToWasm(project: Project, debugInfo: Boolean): TranslationResultWithJsCode {
-    return convertWasmWithConverter(project, debugInfo, kotlinToJSTranslator::doTranslateWithWasm)
-  }
-
-  fun complete(project: Project, line: Int, character: Int): List<Completion> {
-    return kotlinEnvironment.environment {
-      val file = getFilesFrom(project, it).first()
-      try {
-        completionProvider.complete(file, line, character, project.confType, it)
-      } catch (e: Exception) {
-        log.warn("Exception in getting completions. Project: $project", e)
-        emptyList()
-      }
+    fun test(project: Project, addByteCode: Boolean): ExecutionResult {
+        return environment.synchronize { kotlinCompiler.test(project.files, addByteCode) }
+            .also { logExecutionResult(project, it) }
     }
-  }
 
-  fun highlight(project: Project): CompilerDiagnostics = try {
-    when (project.confType) {
-      ProjectType.JAVA, ProjectType.JUNIT -> compileToJvm(project).compilerDiagnostics
-      ProjectType.CANVAS, ProjectType.JS, ProjectType.JS_IR ->
-        convertToJsIr(
-          project,
-        ).compilerDiagnostics
-      ProjectType.WASM, ProjectType.COMPOSE_WASM ->
-        convertToWasm(
-          project,
-          debugInfo = false,
-        ).compilerDiagnostics
+    fun convertToJsIr(project: Project): TranslationJSResult {
+        return convertJsWithConverter(project, kotlinToJSTranslator::doTranslateWithIr)
     }
-  } catch (e: Exception) {
-    log.warn("Exception in getting highlight. Project: $project", e)
-    CompilerDiagnostics(emptyMap())
-  }
 
-  fun getVersion() = version
+    fun compileToJvm(project: Project): CompilationResult<KotlinCompiler.JvmClasses> {
+        return kotlinCompiler.compile(project.files)
+    }
 
-  private fun convertJsWithConverter(
-    project: Project,
-    converter: (List<KtFile>, List<String>) -> CompilationResult<String>
-  ): TranslationJSResult {
-    return kotlinEnvironment.environment { environment ->
-      val files = getFilesFrom(project, environment).map { it.kotlinFile }
-      kotlinToJSTranslator.translateJs(
-        files,
-        project.args.split(" "),
-        converter
-      )
-    }.also { logExecutionResult(project, it) }
-  }
+    fun convertToWasm(project: Project, debugInfo: Boolean): TranslationResultWithJsCode {
+        return convertWasmWithConverter(project, debugInfo, kotlinToJSTranslator::doTranslateWithWasm)
+    }
 
-  private fun convertWasmWithConverter(
-    project: Project,
-    debugInfo: Boolean,
-    converter: (List<KtFile>, List<String>, List<String>, List<String>, Boolean) -> CompilationResult<WasmTranslationSuccessfulOutput>
-  ): TranslationResultWithJsCode {
-    return kotlinEnvironment.environment { environment ->
-      val files = getFilesFrom(project, environment).map { it.kotlinFile }
-      kotlinToJSTranslator.translateWasm(
-        files,
-        debugInfo,
-        project.confType,
-        converter
-      )
-    }.also { logExecutionResult(project, it) }
-  }
+    // TODO(Dmitrii Krasnov): implement this method in KTL-2807
+    @Ignore
+    fun complete(project: Project, line: Int, character: Int): List<Completion> {
+        return emptyList()
+    }
 
-  private fun logExecutionResult(project: Project, executionResult: ExecutionResult) {
-    loggerDetailsStreamer?.logExecutionResult(
-      executionResult,
-      project.confType,
-      getVersion().version
-    )
-  }
+    fun highlight(project: Project): CompilerDiagnostics = try {
+        when (project.confType) {
+            ProjectType.JAVA, ProjectType.JUNIT -> compileToJvm(project).compilerDiagnostics
+            ProjectType.CANVAS, ProjectType.JS, ProjectType.JS_IR ->
+                convertToJsIr(
+                    project,
+                ).compilerDiagnostics
 
-  private fun getFilesFrom(project: Project, coreEnvironment: KotlinCoreEnvironment) = project.files.map {
-    KotlinFile.from(coreEnvironment.project, it.name, it.text)
-  }
+            ProjectType.WASM, ProjectType.COMPOSE_WASM ->
+                convertToWasm(
+                    project,
+                    debugInfo = false,
+                ).compilerDiagnostics
+        }
+    } catch (e: Exception) {
+        log.warn("Exception in getting highlight. Project: $project", e)
+        CompilerDiagnostics(emptyMap())
+    }
+
+    fun getVersion() = version
+
+    private fun convertJsWithConverter(
+        project: Project,
+        converter: (List<ProjectFile>, List<String>) -> CompilationResult<String>
+    ): TranslationJSResult {
+        return environment.synchronize {
+            kotlinToJSTranslator.translateJs(
+                project.files,
+                project.args.split(" "),
+                converter
+            )
+        }
+            .also { logExecutionResult(project, it) }
+    }
+
+    private fun convertWasmWithConverter(
+        project: Project,
+        debugInfo: Boolean,
+        converter: (List<ProjectFile>, List<String>, List<String>, List<String>, Boolean) -> CompilationResult<WasmTranslationSuccessfulOutput>
+    ): TranslationResultWithJsCode {
+
+        return environment.synchronize {
+            kotlinToJSTranslator.translateWasm(
+                project.files,
+                debugInfo,
+                project.confType,
+                converter
+            )
+        }
+            .also { logExecutionResult(project, it) }
+    }
+
+    private fun logExecutionResult(project: Project, executionResult: ExecutionResult) {
+        loggerDetailsStreamer?.logExecutionResult(
+            executionResult,
+            project.confType,
+            getVersion().version
+        )
+    }
+
 }
