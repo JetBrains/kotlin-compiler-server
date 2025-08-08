b81e22f94fc8ec6811c9bf6308551ef90dbd1f84
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2025-08-08 13:31:00 +0200

    KTL-1220: Ignored tests connected to completion.

--- src/test/kotlin/com/compiler/server/ConcurrencyRunnerTest.kt
+++ src/test/kotlin/com/compiler/server/ConcurrencyRunnerTest.kt
@@ -5,77 +5,82 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import kotlin.test.Ignore
 
 class ConcurrencyRunnerTest : BaseExecutorTest() {
-  @Test
-  fun `a lot of hello word test JVM`() {
-    runManyTest {
-      run(
-        code = "fun main() {\n println(\"Hello, world!!!\")\n}",
-        contains = "Hello, world!!!"
-      )
-    }
-  }
-
-  @Test
-  fun `a lot of complete test`() {
-    runManyTest {
-      complete(
-        code = "fun main() {\n    val alex = 1\n    val alex1 = 1 + a\n}",
-        line = 2,
-        character = 21,
-        completions = listOf(
-          "alex"
-        )
-      )
-    }
-  }
-
-  @Test
-  fun `a lot of complete test JS`() {
-    runManyTest {
-      complete(
-        code = "fun main() {\n    val alex = 1\n    val alex1 = 1 + a\n}",
-        line = 2,
-        character = 21,
-        completions = listOf(
-          "alex"
-        ),
-        isJs = true
-      )
-    }
-  }
-
-  @Test
-  fun `a lot of hello word test JS`() {
-    runManyTest {
-      runJsIr(
-        code = "fun main() {\n println(\"Hello, world!!!\")\n}",
-        contains = "println('Hello, world!!!');"
-      )
-    }
-  }
-
-  @Test
-  fun `a lot of hello word test JS IR`() {
-    runManyTest {
-      runJsIr(
-        code = "fun main() {\n println(\"Hello, world!!!\")\n}",
-        contains = "println('Hello, world!!!');"
-      )
-    }
-  }
-
-  private fun runManyTest(times: Int = 100, test: () -> Unit) {
-    runBlocking {
-      launch(Dispatchers.IO) {
-        for (i in 0 until times) {
-          launch(Dispatchers.IO) {
-            test()
-          }
+    @Test
+    fun `a lot of hello word test JVM`() {
+        runManyTest {
+            run(
+                code = "fun main() {\n println(\"Hello, world!!!\")\n}",
+                contains = "Hello, world!!!"
+            )
         }
-      }.join()
     }
-  }
+
+    // TODO(Dmitrii Krasnov): this test is disabled until KTL-2807 is fixed
+    @Ignore
+    @Test
+    fun `a lot of complete test`() {
+        runManyTest {
+            complete(
+                code = "fun main() {\n    val alex = 1\n    val alex1 = 1 + a\n}",
+                line = 2,
+                character = 21,
+                completions = listOf(
+                    "alex"
+                )
+            )
+        }
+    }
+
+    // TODO(Dmitrii Krasnov): this test is disabled until KTL-2807 is fixed
+    @Ignore
+    @Test
+    fun `a lot of complete test JS`() {
+        runManyTest {
+            complete(
+                code = "fun main() {\n    val alex = 1\n    val alex1 = 1 + a\n}",
+                line = 2,
+                character = 21,
+                completions = listOf(
+                    "alex"
+                ),
+                isJs = true
+            )
+        }
+    }
+
+    @Test
+    fun `a lot of hello word test JS`() {
+        runManyTest {
+            runJsIr(
+                code = "fun main() {\n println(\"Hello, world!!!\")\n}",
+                contains = "println('Hello, world!!!');"
+            )
+        }
+    }
+
+    @Test
+    fun `a lot of hello word test JS IR`() {
+        runManyTest {
+            runJsIr(
+                code = "fun main() {\n println(\"Hello, world!!!\")\n}",
+                contains = "println('Hello, world!!!');"
+            )
+        }
+    }
+
+    private fun runManyTest(times: Int = 100, test: () -> Unit) {
+        runBlocking {
+            launch(Dispatchers.IO) {
+                for (i in 0 until times) {
+                    launch(Dispatchers.IO) {
+                        test()
+                    }
+                }
+            }.join()
+        }
+    }
 
 }
