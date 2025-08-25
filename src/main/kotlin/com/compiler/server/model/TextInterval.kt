708e205a520f339f106965f74b1377ba100cac74
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2025-08-25 16:16:16 +0200

    small fixes

--- src/main/kotlin/com/compiler/server/model/TextInterval.kt
+++ src/main/kotlin/com/compiler/server/model/TextInterval.kt
@@ -1,29 +1,9 @@
 package com.compiler.server.model
 
-import com.intellij.openapi.editor.Document
 
 data class TextInterval(val start: TextPosition, val end: TextPosition) {
   data class TextPosition(val line: Int, val ch: Int) : Comparable<TextPosition> {
     override fun compareTo(other: TextPosition): Int = compareValuesBy(this, other, { it.line }, { it.ch })
   }
 
-  companion object {
-    fun from(start: Int, end: Int, currentDocument: Document): TextInterval {
-      val lineNumberForElementStart = currentDocument.getLineNumber(start)
-      val lineNumberForElementEnd = currentDocument.getLineNumber(end)
-      var charNumberForElementStart = start - currentDocument.getLineStartOffset(lineNumberForElementStart)
-      var charNumberForElementEnd = end - currentDocument.getLineStartOffset(lineNumberForElementStart)
-      if ((start == end) && (lineNumberForElementStart == lineNumberForElementEnd)) {
-        charNumberForElementStart--
-        if (charNumberForElementStart < 0) {
-          charNumberForElementStart++
-          charNumberForElementEnd++
-        }
-      }
-      return TextInterval(
-        TextPosition(lineNumberForElementStart, charNumberForElementStart),
-        TextPosition(lineNumberForElementEnd, charNumberForElementEnd)
-      )
-    }
-  }
 }
