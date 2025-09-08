137f05276e7353fe448c2e861f84273523cf0902
Zofia Wiora <zofia.wiora@gmail.com>
2025-09-08 10:04:27 +0200

    Clean compilation code

--- src/main/kotlin/com/compiler/server/compiler/components/CompilationLogger.kt
+++ src/main/kotlin/com/compiler/server/compiler/components/CompilationLogger.kt
@@ -5,65 +5,53 @@ import com.compiler.server.model.ProjectSeveriry
 import com.compiler.server.model.TextInterval
 import org.jetbrains.kotlin.buildtools.api.KotlinLogger
 
-class CompilationLogger : KotlinLogger {
-    override val isDebugEnabled: Boolean = false
+class CompilationLogger(
+    override val isDebugEnabled: Boolean = false,
+) : KotlinLogger {
 
-    var warnings: Map<String, MutableList<ErrorDescriptor>> = emptyMap()
+    var compilationLogs: Map<String, MutableList<ErrorDescriptor>> = emptyMap()
 
     override fun debug(msg: String) {
-
+        if (isDebugEnabled) println("[DEBUG] $msg")
     }
 
     override fun error(msg: String, throwable: Throwable?) {
+        if (isDebugEnabled) System.err.println("[ERROR] $msg" + (throwable?.let { ": ${it.message}" } ?: ""))
         try {
-            val path = msg.split(" ")[0]
-            val className = path.split("/").last().split(".").first()
-            val message = msg.split(path)[1].drop(1)
-            val splitPath = path.split(":")
-            val line = splitPath[splitPath.size - 4].toInt() - 1
-            val ch = splitPath[splitPath.size - 3].toInt() - 1
-            val endLine = splitPath[splitPath.size - 2].toInt() - 1
-            val endCh = splitPath[splitPath.size - 1].toInt() - 1
-            val ed = ErrorDescriptor(
-                TextInterval(TextInterval.TextPosition(line, ch), TextInterval.TextPosition(endLine, endCh)),
-//                TextInterval(TextInterval.TextPosition(endLine, endCh), TextInterval.TextPosition(endLine, endCh)),
-                message,
-                ProjectSeveriry.ERROR,
-                className
-            )
-            warnings["$className.kt"]?.add(ed)
-        } catch (_: Exception) {
-        }
+            addCompilationLog(msg, ProjectSeveriry.ERROR, classNameOverride = null)
+        } catch (_: Exception) {}
     }
 
     override fun info(msg: String) {
-
+        if (isDebugEnabled) println("[INFO] $msg")
     }
 
     override fun lifecycle(msg: String) {
-
+        if (isDebugEnabled) println("[LIFECYCLE] $msg")
     }
 
     override fun warn(msg: String, throwable: Throwable?) {
+        if (isDebugEnabled) System.err.println("[WARN] $msg" + (throwable?.let { ": ${it.message}" } ?: ""))
         try {
-            val path = msg.split(" ")[0]
-            val className = path.split("/").last().split(".").first()
-            val message = msg.split(path)[1].drop(1)
-            val splitPath = path.split(":")
-            val line = splitPath[splitPath.size - 4].toInt() - 1
-            val ch = splitPath[splitPath.size - 3].toInt() - 1
-            val endLine = splitPath[splitPath.size - 2].toInt() - 1
-            val endCh = splitPath[splitPath.size - 1].toInt() - 1
-            val ed = ErrorDescriptor(
-                TextInterval(TextInterval.TextPosition(line, ch), TextInterval.TextPosition(endLine, endCh)),
-//                TextInterval(TextInterval.TextPosition(endLine, endCh), TextInterval.TextPosition(endLine, endCh)),
-                message,
-                ProjectSeveriry.WARNING,
-                "WARNING"
-            )
-            warnings["$className.kt"]?.add(ed)
-        } catch (_: Exception) {
-        }
+            addCompilationLog(msg, ProjectSeveriry.WARNING, classNameOverride = "WARNING")
+        } catch (_: Exception) {}
     }
 
+    private fun addCompilationLog(msg: String, severity: ProjectSeveriry, classNameOverride: String?) {
+        val path = msg.split(" ")[0]
+        val className = path.split("/").last().split(".").first()
+        val message = msg.split(path)[1].drop(1)
+        val splitPath = path.split(":")
+        val line = splitPath[splitPath.size - 4].toInt() - 1
+        val ch = splitPath[splitPath.size - 3].toInt() - 1
+        val endLine = splitPath[splitPath.size - 2].toInt() - 1
+        val endCh = splitPath[splitPath.size - 1].toInt() - 1
+        val ed = ErrorDescriptor(
+            TextInterval(TextInterval.TextPosition(line, ch), TextInterval.TextPosition(endLine, endCh)),
+            message,
+            severity,
+            classNameOverride ?: className
+        )
+        compilationLogs["$className.kt"]?.add(ed)
+    }
 }
