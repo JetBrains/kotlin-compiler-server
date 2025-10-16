7262b1ef08a31cb1ee2566c307744e533cf5a7b9
Dmitrii Krasnov <dmitrii.krasnov@jetbrains.com>
2025-10-16 15:26:29 +0200

    KTL-2973 Deprecated old compilation endpoint.

--- src/main/kotlin/com/compiler/server/controllers/KotlinPlaygroundRestController.kt
+++ src/main/kotlin/com/compiler/server/controllers/KotlinPlaygroundRestController.kt
@@ -1,9 +1,6 @@
 package com.compiler.server.controllers
 
-import com.compiler.server.exceptions.LegacyJsException
 import com.compiler.server.model.Project
-import com.compiler.server.model.ProjectType
-import com.compiler.server.service.KotlinProjectExecutor
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,53 +9,36 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class KotlinPlaygroundRestController(private val kotlinProjectExecutor: KotlinProjectExecutor) {
+@Deprecated(
+    "Use only for old kotlin playground widget. " +
+            "Should be removed in KTL-2974 around 2.5.0"
+)
+class KotlinPlaygroundRestController() {
 
-  /**
-   * Endpoint for support requests from kotlin playground client.
-   * Kotlin Playground component see: https://github.com/JetBrains/kotlin-playground
-   * Old server see: https://github.com/JetBrains/kotlin-web-demo
-   */
-  @RequestMapping(
-    value = ["/kotlinServer"],
-    method = [RequestMethod.GET, RequestMethod.POST],
-    consumes = [MediaType.ALL_VALUE],
-    produces = [MediaType.APPLICATION_JSON_VALUE]
-  )
-  @Suppress("IMPLICIT_CAST_TO_ANY")
-  fun tryKotlinLangObsoleteEndpoint(
-    @RequestParam type: String,
-    @RequestParam(required = false) line: Int?,
-    @RequestParam(required = false) ch: Int?,
-    @RequestParam(required = false) project: Project?,
-    @RequestParam(defaultValue = "false") addByteCode: Boolean,
-  ): ResponseEntity<*> {
-    val result = when (type) {
-      "getKotlinVersions" -> listOf(kotlinProjectExecutor.getVersion())
-      else -> {
-        if (project == null) error("No parameter 'project' found")
-        when (type) {
-          "run" -> {
-            when (project.confType) {
-              ProjectType.JAVA -> kotlinProjectExecutor.run(project, addByteCode)
-              ProjectType.JS -> throw LegacyJsException()
-              ProjectType.JS_IR, ProjectType.CANVAS ->
-                kotlinProjectExecutor.convertToJsIr(
-                  project,
-                )
-              ProjectType.WASM, ProjectType.COMPOSE_WASM -> kotlinProjectExecutor.convertToWasm(
-                project,
-                debugInfo = false,
-              )
-              ProjectType.JUNIT -> kotlinProjectExecutor.test(project, addByteCode)
-            }
-          }
-
-          "highlight" -> kotlinProjectExecutor.highlight(project)
-          else -> error("No parameter 'type' found")
-        }
-      }
-    }
-    return ResponseEntity.ok(result)
-  }
+    /**
+     * Endpoint for support requests from kotlin playground client.
+     * Kotlin Playground component see: https://github.com/JetBrains/kotlin-playground
+     * Old server see: https://github.com/JetBrains/kotlin-web-demo
+     */
+    @RequestMapping(
+        value = ["/kotlinServer"],
+        method = [RequestMethod.GET, RequestMethod.POST],
+        consumes = [MediaType.ALL_VALUE],
+    )
+    @Suppress("IMPLICIT_CAST_TO_ANY")
+    fun tryKotlinLangObsoleteEndpoint(
+        @RequestParam type: String,
+        @RequestParam(required = false) line: Int?,
+        @RequestParam(required = false) ch: Int?,
+        @RequestParam(required = false) project: Project?,
+        @RequestParam(defaultValue = "false") addByteCode: Boolean,
+    ): ResponseEntity<String> =
+        ResponseEntity
+            .badRequest()
+            .body(
+                "This endpoint is not available anymore.\n" +
+                        "And will be soon removed from the Kotlin Playground.\n" +
+                        "If you are using playground widget, then update to the new version: https://github.com/JetBrains/kotlin-playground \n" +
+                        "Otherwise, use the new api available at: https://api.kotlinlang.org/api/<kotlin_version>/compiler/run .\n"
+            )
 }
