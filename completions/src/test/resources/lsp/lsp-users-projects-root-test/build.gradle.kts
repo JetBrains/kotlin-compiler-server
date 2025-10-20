b24418a2630c96a541203f231a0e7e4c749b4d82
Stefano Furi <stefano.furi@jetbrains.com>
2025-10-20 09:35:29 +0200

    chore: add jackson dependency for `ImportTest`

--- completions/src/test/resources/lsp/lsp-users-projects-root-test/build.gradle.kts
+++ completions/src/test/resources/lsp/lsp-users-projects-root-test/build.gradle.kts
@@ -8,4 +8,5 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.2")
 }
