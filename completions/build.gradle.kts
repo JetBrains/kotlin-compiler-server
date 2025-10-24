8d3d6090ca861ed916006aafa5cbf5a76f5f2943
Stefano Furi <stefano.furi@jetbrains.com>
2025-10-24 17:09:42 +0200

    feat: containerize `Completion` application
    the docker compose explicitly sets in the same private network the
    kotlin-lsp dependency. Parameters are configurable through the bash
    script.

--- completions/build.gradle.kts
+++ completions/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
 plugins {
     id("base-spring-boot-conventions")
 }
+
+version = "${libs.versions.kotlin.get()}-SNAPSHOT"
 
 dependencies {
     implementation(libs.spring.boot.starter.webflux)
@@ -9,4 +13,12 @@ dependencies {
     testImplementation(libs.kotlin.test)
     testImplementation(libs.bundles.testcontainers)
     testImplementation(libs.rector.test)
+}
+
+tasks.named<BootBuildImage>("bootBuildImage") {
+    val baseImageName = "sfuri/kotlin-compiler-server-completions-lsp"
+    // TODO: push docker image to JB registry
+    // publish = true
+    imageName = "$baseImageName:${project.version}"
+    tags = setOf("$baseImageName:latest")
 }
