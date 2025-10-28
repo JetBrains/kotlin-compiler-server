ef40a10367e20944bc6015b83cffe5c55e6bee3f
Stefano Furi <stefano.furi@jetbrains.com>
2025-10-28 12:20:35 +0100

    chore: set up springdoc for completions subproject

--- completions/build.gradle.kts
+++ completions/build.gradle.kts
@@ -9,6 +9,7 @@ version = "${libs.versions.kotlin.get()}-SNAPSHOT"
 dependencies {
     implementation(libs.spring.boot.starter.webflux)
     implementation(libs.spring.boot.docker.compose)
+    implementation(libs.springdoc.webflux)
     implementation(libs.org.eclipse.lsp4j)
     implementation(libs.kotlinx.coroutines.reactor)
     testImplementation(libs.kotlin.test)
