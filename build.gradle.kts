75831648be30d061f9a71218c141274ffe1800d9
Stefano Furi <stefano.furi@jetbrains.com>
2025-10-28 12:20:06 +0100

    refactor: distinguish between springdoc versions (webmvc or webflux)

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation(libs.aws.springboot.container)
-    implementation(libs.springdoc)
+    implementation(libs.springdoc.webmvc)
     implementation(libs.gson)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlin.compiler.arguments.description)
