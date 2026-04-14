323efa4ab4be9244891365b0d81a810ec139243f
Nikita Vlaev <nikita.vlaev@jetbrains.com>
2026-04-14 21:28:20 +0200

    KTL-1731: Add cache proxy lambda

---
+++ cache-proxy/src/main/kotlin/com/compiler/server/cacheproxy/dto/ComposeWasmRequest.kt
@@ -0,0 +1,13 @@
+package com.compiler.server.cacheproxy.dto
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class ProjectRequest(
+    val args: String = "",
+    val files: List<FileDto> = listOf(),
+    val compilerArguments: List<Map<String, Any>> = emptyList(),
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class FileDto(val text: String = "", val name: String = "")
